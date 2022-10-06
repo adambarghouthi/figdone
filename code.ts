7a26ca2bc9581ef147e380560b6d994f7d4f8b74
Adam Albarghouthi <adambargh@gmail.com>
2022-10-05 21:20:14 -0400

    add code for figjam

--- code.ts
+++ code.ts
@@ -83,4 +83,58 @@ if (figma.editorType === 'figma') {
 // If the plugins isn't run in Figma, run this code
 } else {
   
+  // This shows the HTML page in "ui.html".
+  figma.showUI(__html__);
+  figma.ui.resize(300, 390);
+
+  updateFrames();
+
+  figma.on("selectionchange", () => {
+    const { selection } = figma.currentPage;
+
+    if (selection.length === 1) {
+      const frame = figma.currentPage.children.find(
+        (f) => f.id === selection[0].id
+      );
+
+      if (frame) {
+        scrollToFrame(frame.id);
+      }
+    }
+  });
+
+  // Calls to "parent.postMessage" from within the HTML page will trigger this
+  // callback. The callback will be passed the "pluginMessage" property of the
+  // posted message.
+
+  figma.ui.onmessage = (msg) => {
+    // One way of distinguishing between different types of messages sent from
+    // your HTML page is to use an object with a "type" property like this.
+    if (msg.type === "update-status") {
+      const { frameId, newStatusIcon } = msg;
+
+      figma.currentPage.children.forEach((f) => {
+        if (f.id === frameId) {
+          const regex = /\[(.*?)]/g;
+          const strippedName = f.name.replace(regex, "").trim();
+
+          f.name = `[${newStatusIcon}] ${strippedName}`;
+          updateFrames();
+        }
+      });
+    }
+
+    if (msg.type === "focus-frame") {
+      const { frameId } = msg;
+      const frame = figma.currentPage.children.find((f) => f.id === frameId);
+      if (frame) {
+        figma.viewport.scrollAndZoomIntoView([frame]);
+      }
+    }
+
+    // // Make sure to close the plugin when you're done. Otherwise the plugin will
+    // // keep running, which shows the cancel button at the bottom of the screen.
+    // figma.closePlugin();
+  };
+  
 };
