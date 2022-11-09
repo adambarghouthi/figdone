b805d8a3c2bf8dfb9c3ea160948394decc5468d8
Adam Albarghouthi <adambargh@gmail.com>
2022-11-09 16:34:16 -0500

    add permissions currentuser

--- build-figma-plugin.manifest.js
+++ build-figma-plugin.manifest.js
@@ -1,8 +1,9 @@
 module.exports = function (manifest) {
-    // ...
-    return {
-        ...manifest,
-        name: 'FigDone',
-        id: '1159865715100399885'
-    }
-}
+  // ...
+  return {
+    ...manifest,
+    name: "FigDone",
+    id: "1159865715100399885",
+    permissions: ["currentuser"],
+  };
+};
